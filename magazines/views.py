b06422ed64560971118397b02112d1e0f8442836
Clare <clarelpidgeon@yahoo.co.uk>
2017-06-21 10:32:56 +0100

    added style and fixed subscribe view

--- magazines/views.py
+++ magazines/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required(login_url='/accounts/login')
+@login_required(login_url='/login/')
 def all_magazines(request):
     magazines = Magazine.objects.all()
     return render(request, 'magazines/magazines.html', {'magazines': magazines})
