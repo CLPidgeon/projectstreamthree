471b1f27152592d88fe3da34b7b181ceec9aa64f
Clare <clarelpidgeon@yahoo.co.uk>
2017-10-09 14:33:10 +0100

    settings update

--- settings/dev.py
+++ settings/dev.py
@@ -17,3 +17,6 @@ DISQUS_WEBSITE_SHORTNAME = 'icehockeysite'
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_jdJD1T7I9smh6Qem2d7eJIXP')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_vfYulusugV6nOunj56tb5ELk')
+
+
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
