bb53b4bd05eaf49bb40c3b9168d0fe8d6a69d131
Clare <clarelpidgeon@yahoo.co.uk>
2017-10-17 14:35:11 +0100

    home code tidy up

--- home/views.py
+++ home/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render
 
 
 def get_index(request):
+    "Gets home page"
     return render(request, 'index.html')
 
 
 def fantasy(request):
+    "Gets fantasy ice hockey page"
     return render(request, 'fantasyIH.html')
 
 
 def get_about(request):
+    "Gets about page"
     return render(request, 'about.html')
 
 
 def get_contact(request):
+    "Gets contact page"
     return render(request, 'contact.html')
