f9aea8183b1c7d0dcac3e10c7c6364811f68fce6
Clare <clarelpidgeon@yahoo.co.uk>
2017-10-20 12:19:04 +0100

    added disqus

--- settings/staging.py
+++ settings/staging.py
@@ -7,6 +7,9 @@ DATABASES = {
     'default': dj_database_url.config('CLEARDB_DATABASE_URL')
 }
 
+# Disqus
+DISQUS_WEBSITE_SHORTNAME = 'icehockeysite'
+
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', 'pk_test_jdJD1T7I9smh6Qem2d7eJIXP')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET', 'sk_test_vfYulusugV6nOunj56tb5ELk')
